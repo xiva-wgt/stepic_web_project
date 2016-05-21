4dc9c1ba7da2367b43531c6d313d4a1bbffa4c1c
xiva-wgt <xiva-wgt@mail.ru>
2016-05-21 21:13:31 +0300

    commit for 2.1 attempt #1

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hzjm7rrsan8wk(8gq1u#lfg2dn2$ce2ak2@a%xmyo61&d(oqfn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
