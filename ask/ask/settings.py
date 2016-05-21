7e55d8298fa7d47fd30c23a216dac883153b1f32
xiva-wgt <xiva-wgt@mail.ru>
2016-05-22 02:49:25 +0300

    commit for 2.3 attempt #2

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -89,7 +89,7 @@ else:
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'djdb',
             'USER': 'django',
-            'PASSWORD': 'qwertypass',
+            'PASSWORD': 'qwertypass123',
             'HOST': 'localhost',
             'PORT': '3306',
         }
--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@
 
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE djdb;"
-mysql -uroot -e "CREATE USER 'django@%' IDENTIFIED BY 'qwertypass';"
+mysql -uroot -e "CREATE USER 'django@%' IDENTIFIED BY 'qwertypass123';"
 mysql -uroot -e "GRANT ALL ON djdb.* TO 'django@%';"
 mysql -uroot -e "GRANT USAGE ON *.* TO 'django@%';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
