94954e60a038d7ef3f30dde5aa52ab7eeb73554e
xiva-wgt <xiva-wgt@users.noreply.github.com>
2016-05-21 04:50:04 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python
 import urlparse
 
-def app_start(environ, start_response):
+def application(environ, start_response):
     response_body = b''
     dict_with_query_string = urlparse.parse_qs(environ.get('QUERY_STRING', ''), keep_blank_values=True)
     for item in dict_with_query_string:
         for item_value in dict_with_query_string[item]:
             response_body += b'{0}={1}\n'.format(item, item_value)
     status = '200 OK'
-
-    response_headers = [
-        ('Content-Type', 'text/plain'),
-    ]
-
-    start_response(status, response_headers)
-
+    start_response(status, [('Content-Type', 'text/plain')])
     return [response_body]
 
