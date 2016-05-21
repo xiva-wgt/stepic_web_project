fceddb15c23ac7e2852b4ebc45b97791f156ec95
xiva-wgt <xiva-wgt@mail.ru>
2016-05-21 21:18:21 +0300

    commit for 2.1 attempt #3 enable all host in allowed_host for fix error message 'CommandError: You must set settings.ALLOWED_HOSTS if DEBUG is False' for test.

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hzjm7rrsan8wk(8gq1u#lfg2dn2$ce2ak2@a%xmyo61&d(oqfn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
