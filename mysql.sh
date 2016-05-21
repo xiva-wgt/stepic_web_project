9155cfb2423dd8e099ed57e05442e2d39c2aa099
xiva-wgt <xiva-wgt@mail.ru>
2016-05-22 01:18:17 +0300

    commit for 2.3 attempt #1

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
 sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE djbase;"
-mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass123';"
-mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
-
-sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE djbase;"
-mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass123';"
-mysql -uroot -e "GRANT ALL ON djbase.* TO 'django@localhost';"
+mysql -uroot -e "CREATE DATABASE djdb;"
+mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'qwertypass';"
+mysql -uroot -e "GRANT ALL ON djdb.* TO 'django@localhost';"
+mysql -uroot -e "GRANT USAGE ON *.* TO 'django@localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
