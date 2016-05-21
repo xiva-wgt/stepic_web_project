0740441fdf1a32ed687e396a65413d0a7d8ff63f
xiva-wgt <xiva-wgt@users.noreply.github.com>
2016-05-21 05:01:27 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo gunicorn -b 0.0.0.0:8080 --daemon -c /etc/gunicorn.d/hello.py hello:application
 sudo /etc/init.d/gunicorn restart
