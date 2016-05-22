df4c1f440da46ecd36c625e84989528089653137
xiva-wgt <xiva-wgt@mail.ru>
2016-05-23 02:37:12 +0300

    commit for 2.6 attempt #5

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -99,6 +99,7 @@ def ask(request):
     if request.method == "POST":
         form = AskForm(request.POST)
         if form.is_valid():
+            form._user = request.user
             post = form.save()
             url = post.get_url()
             return HttpResponseRedirect(url)
