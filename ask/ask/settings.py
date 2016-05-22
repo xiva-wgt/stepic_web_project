48998c2bfa511307ae2a3cb0e0252b9e2eb0cb39
xiva-wgt <xiva-wgt@mail.ru>
2016-05-22 22:51:59 +0300

    commit for 2.5 attempt #2

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
+    #'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
