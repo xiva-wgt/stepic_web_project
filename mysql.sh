5b306a7b1d15b62e646164024d96868a82d3a88c
xiva-wgt <xiva-wgt@mail.ru>
2016-05-22 03:04:07 +0300

    commit for 2.3 attempt #2

--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,5 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE djdb;"
 mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'qwertypass123';"
-mysql -uroot -e "GRANT ALL ON djdb.* TO 'django@localhost';"
-mysql -uroot -e "GRANT USAGE ON *.* TO 'django@localhost';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON djdb.* TO 'django@localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
