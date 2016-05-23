a585582789f8e3dc1045170b0f0300c85905b741
xiva-wgt <xiva-wgt@mail.ru>
2016-05-24 00:45:37 +0300

    for debug

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -95,12 +95,16 @@ def question(request, pk_question):
 def ask(request):
     if request.method == "POST":
         form = AskForm(request.POST)
-        if form.is_valid():
-            post = form.save()
-            #redirect_url = post.get_url()
-            redirect_url = post.get_absolute_url()
-            #redirect_url = reverse('question', args=[post.id])
-            return HttpResponseRedirect(redirect_url)
+        try:
+            if form.is_valid():
+                post = form.save()
+                #redirect_url = post.get_url()
+                redirect_url = post.get_absolute_url()
+                #redirect_url = reverse('question', args=[post.id])
+                print redirect_url
+                return HttpResponseRedirect(redirect_url)
+        except Exception as e:
+            print str(e)
     else:
         form = AskForm()
     return render(request, 'ask.html', {'form': form})
